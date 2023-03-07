bcad3baffd3fb9d085bc1f6de9ca02a9f0509274
zzakrzewska <95741647+zzakrzewska@users.noreply.github.com>
2023-03-07 09:40:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Zofia Zakrzewska / 12c / s24365
+
+print('wpisz dzialanie (format [x + y]: ')
+input = input()
+
+x = input.split()
+
+try:
+    if x[1] == '+':
+        print(int(x[0]) + int(x[2]))
+    elif x[1] == '-':
+        print(int(x[0]) - int(x[2]))
+    elif x[1] == '*':
+        print(int(x[0]) * int(x[2]))
+    elif x[1] == '/':
+        print(int(x[0]) / int(x[2]))
+    else:
+        print('nieprawidlowy input')
+
+except(IndexError, ValueError):
+    print('nieprawidlowy input')
+
+exit()
